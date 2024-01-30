5c5382c7714d1aac7dff7e9c8aa330337f89fc73
unknown <dagmfre@gmail.com>
2024-01-31 01:58:33 +0300

    Updating and correct the projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -28,10 +28,15 @@ const Projects = () => {
         <div className="pros">
           <div className="pros-front">
             <div>
-              <h4>Netflix Clone</h4>
+              <h4>Nova-Cars-An-Ecommerce-Website</h4>
               <p>
-                A minimal, dark blue theme for VS Code, Sublime Text, Atom,
-                iTerm, and more. Available on Visual Studio Marketplace
+                Nova-Cars is a MERN stack website designed and built for
+                effortless car exploration and purchase. It allows users
+                navigate the platform easily to find and reserve a wide variety
+                of cars, including SUVs, trucks, and vans...
+                <i className="click">
+                  (click the 'search' icon below to learn more)
+                </i>
               </p>
             </div>
             <div className="pros-desc">
@@ -43,9 +48,16 @@ const Projects = () => {
                 >
                   <i className="fa-solid fa-arrow-up-right-from-square"></i>
                 </a>
+                <a
+                  rel="noreferrer"
+                  target="_blank"
+                  href="https://github.com/dagmfre/Nova-Cars-An-Ecommerce-Website"
+                >
+                  <i className="fa-brands fa-github"></i>
+                </a>
                 <div id="proj-image-gallery" href="/#">
                   <a
-                    href="nova.png"
+                    href="nova-descrp.png"
                     data-pswp-width={"1320"}
                     data-pswp-height={"614.5546 pt"}
                     rel="noreferrer"
@@ -54,37 +66,47 @@ const Projects = () => {
                     <i className="fa-solid fa-magnifying-glass-plus"></i>
                   </a>
                   <a
+                    href="nova.png"
+                    data-pswp-width={"1320"}
+                    data-pswp-height={"614.5546 pt"}
+                    rel="noreferrer"
+                    target="_blank"
+                  ></a>
+                  <a
                     href="nova2.png"
                     data-pswp-width={"1320"}
                     data-pswp-height={"614.5546"}
                     rel="noreferrer"
                     target="_blank"
-                  >
-                  </a>
+                  ></a>
                   <a
                     href="nova3.png"
                     data-pswp-width={"1320 "}
                     data-pswp-height={"920.5812"}
                     rel="noreferrer"
                     target="_blank"
-                  >
-                  </a>
+                  ></a>
                   <a
                     href="nova4.png"
                     data-pswp-width={"1320"}
                     data-pswp-height={"735"}
                     rel="noreferrer"
                     target="_blank"
-                  >
-                  </a>
+                  ></a>
                   <a
                     href="nova5.png"
                     data-pswp-width={"1320"}
                     data-pswp-height={"610.1887"}
                     rel="noreferrer"
                     target="_blank"
-                  >
-                  </a>
+                  ></a>
+                  <a
+                    href="nova-mongo.png"
+                    data-pswp-width={"1320"}
+                    data-pswp-height={"610.1887"}
+                    rel="noreferrer"
+                    target="_blank"
+                  ></a>
                 </div>
               </div>
               <div>
@@ -102,279 +124,108 @@ const Projects = () => {
         <div className="pros">
           <div className="pros-front">
             <div>
-              <h4>Netflix Clone</h4>
+              <h4>Complete Netflix Clone</h4>
               <p>
-                A minimal, dark blue theme for VS Code, Sublime Text, Atom,
-                iTerm, and more. Available on Visual Studio Marketplace
+                This project is a complete Netflix clone built with the MERN
+                stack and powered by the TMDB API. It contains features like
+                searching movies and TV shows, adding them to the watchlist...
+                <i className="click">
+                  (click the 'search' icon below to learn more)
+                </i>
               </p>
             </div>
             <div className="pros-desc">
               <div>
-                <a href="" target="_blank">
+                <a href="/#" rel="noreferrer" target="_blank">
                   <i className="fa-solid fa-arrow-up-right-from-square"></i>
                 </a>
-
-                <div id={"proj-image-gallery"} href="/">
+                <a
+                  rel="noreferrer"
+                  target="_blank"
+                  href="https://github.com/dagmfre/Netlix_Clone_App"
+                >
+                  <i className="fa-brands fa-github"></i>
+                </a>
+                <div id="proj-image-gallery" href="/#">
                   <a
-                    href="nova.png"
+                    href="netflix-descrp.png"
                     data-pswp-width={"1320"}
-                    data-pswp-height={"614.5546"}
+                    data-pswp-height={"614.5546 pt"}
                     rel="noreferrer"
                     target="_blank"
                   >
                     <i className="fa-solid fa-magnifying-glass-plus"></i>
                   </a>
+                  <a
+                    href="netflix4.png"
+                    data-pswp-width={"1320"}
+                    data-pswp-height={"614.5546 pt"}
+                    rel="noreferrer"
+                    target="_blank"
+                  ></a>
+                  <a
+                    href="netflix5.png"
+                    data-pswp-width={"1320"}
+                    data-pswp-height={"614.5546 pt"}
+                    rel="noreferrer"
+                    target="_blank"
+                  ></a>
+                  <a
+                    href="netflix-a.png"
+                    data-pswp-width={"1320"}
+                    data-pswp-height={"614.5546"}
+                    rel="noreferrer"
+                    target="_blank"
+                  ></a>
+                  <a
+                    href="netflix-b.png"
+                    data-pswp-width={"1320"}
+                    data-pswp-height={"614.5546"}
+                    rel="noreferrer"
+                    target="_blank"
+                  ></a>
+                  <a
+                    href="netflix.png"
+                    data-pswp-width={"1320"}
+                    data-pswp-height={"614.5546"}
+                    rel="noreferrer"
+                    target="_blank"
+                  ></a>
+                  <a
+                    href="netflix2.png"
+                    data-pswp-width={"1320 "}
+                    data-pswp-height={"614.5546 pt"}
+                    rel="noreferrer"
+                    target="_blank"
+                  ></a>
+                  <a
+                    href="netflix3.png"
+                    data-pswp-width={"1320"}
+                    data-pswp-height={"614.5546 pt"}
+                    rel="noreferrer"
+                    target="_blank"
+                  ></a>
+                  <a
+                    href="netflix6.png"
+                    data-pswp-width={"1320"}
+                    data-pswp-height={"614.5546 pt"}
+                    rel="noreferrer"
+                    target="_blank"
+                  ></a>
                 </div>
               </div>
               <div>
                 <p>React</p>
                 <p>Node</p>
                 <p>Express</p>
-                <p>Git</p>
-                <p>Netlify</p>
+                <br />
+                <p>mongoDB</p>
+                <p>Passport.js</p>
+                <p>Bootsrap</p>
               </div>
             </div>
           </div>
-          <img className="pros-img" src="p3.jpg" alt="" />
-          <div className="pros-back"></div>
-        </div>
-
-        <div className="pros">
-          <div className="pros-front">
-            <div>
-              <h4>Netflix Clone</h4>
-              <p>
-                A minimal, dark blue theme for VS Code, Sublime Text, Atom,
-                iTerm, and more. Available on Visual Studio Marketplace
-              </p>
-            </div>
-            <div className="pros-desc">
-              <div>
-                <a href="" target="_blank">
-                  <i className="fa-solid fa-arrow-up-right-from-square"></i>
-                </a>
-
-                <div id={"proj-image-gallery"} href="/">
-                  <a
-                    href="nova.png"
-                    data-pswp-width={"1320"}
-                    data-pswp-height={"614.5546"}
-                    rel="noreferrer"
-                    target="_blank"
-                  >
-                    <i className="fa-solid fa-magnifying-glass-plus"></i>
-                  </a>
-                </div>
-              </div>
-              <div>
-                <p>React</p>
-                <p>Node</p>
-                <p>Express</p>
-                <p>Git</p>
-                <p>Netlify</p>
-              </div>
-            </div>
-          </div>
-          <img className="pros-img" src="p.jpg" alt="" />
-          <div className="pros-back"></div>
-        </div>
-
-        <div className="pros">
-          <div className="pros-front">
-            <div>
-              <h4>Netflix Clone</h4>
-              <p>
-                A minimal, dark blue theme for VS Code, Sublime Text, Atom,
-                iTerm, and more. Available on Visual Studio Marketplace
-              </p>
-            </div>
-            <div className="pros-desc">
-              <div>
-                <a href="" target="_blank">
-                  <i className="fa-solid fa-arrow-up-right-from-square"></i>
-                </a>
-                <div id={"proj-image-gallery"} href="/">
-                  <a
-                    href="nova.png"
-                    data-pswp-width={"1320"}
-                    data-pswp-height={"614.5546"}
-                    rel="noreferrer"
-                    target="_blank"
-                  >
-                    <i className="fa-solid fa-magnifying-glass-plus"></i>
-                  </a>
-                </div>
-              </div>
-              <div>
-                <p>React</p>
-                <p>Node</p>
-                <p>Express</p>
-                <p>Git</p>
-                <p>Netlify</p>
-              </div>
-            </div>
-          </div>
-          <img className="pros-img" src="p2.jpg" alt="" />
-          <div className="pros-back"></div>
-        </div>
-      </div>
-
-      <div className="pros-cont" data-aos="zoom-up">
-        <div className="pros">
-          <div className="pros-front">
-            <div>
-              <h4>Netflix Clone</h4>
-              <p>
-                A minimal, dark blue theme for VS Code, Sublime Text, Atom,
-                iTerm, and more. Available on Visual Studio Marketplace
-              </p>
-            </div>
-
-            <div className="pros-desc">
-              <div>
-                <a href="" target="_blank">
-                  <i className="fa-solid fa-arrow-up-right-from-square"></i>
-                </a>
-
-                <div id={"proj-image-gallery"} href="/">
-                  <a
-                    href="nova.png"
-                    data-pswp-width={"1320"}
-                    data-pswp-height={"614.5546"}
-                    rel="noreferrer"
-                    target="_blank"
-                  >
-                    <i className="fa-solid fa-magnifying-glass-plus"></i>
-                  </a>
-                </div>
-              </div>
-              <div>
-                <p>React</p>
-                <p>Node</p>
-                <p>Express</p>
-                <p>Git</p>
-                <p>Netlify</p>
-              </div>
-            </div>
-          </div>
-          <img className="pros-img" src="tesla.jpg" alt="" srcSet="" />
-          <div className="pros-back"></div>
-        </div>
-        <div className="pros">
-          <div className="pros-front">
-            <div>
-              <h4>Netflix Clone</h4>
-              <p>
-                A minimal, dark blue theme for VS Code, Sublime Text, Atom,
-                iTerm, and more. Available on Visual Studio Marketplace
-              </p>
-            </div>
-            <div className="pros-desc">
-              <div>
-                <a href="" target="_blank">
-                  <i className="fa-solid fa-arrow-up-right-from-square"></i>
-                </a>
-                <div id={"proj-image-gallery"} href="/">
-                  <a
-                    href="nova.png"
-                    data-pswp-width={"1320"}
-                    data-pswp-height={"614.5546"}
-                    rel="noreferrer"
-                    target="_blank"
-                  >
-                    <i className="fa-solid fa-magnifying-glass-plus"></i>
-                  </a>
-                </div>
-              </div>
-              <div>
-                <p>React</p>
-                <p>Node</p>
-                <p>Express</p>
-                <p>Git</p>
-                <p>Netlify</p>
-              </div>
-            </div>
-          </div>
-          <img className="pros-img" src="p3.jpg" alt="" />
-          <div className="pros-back"></div>
-        </div>
-
-        <div className="pros">
-          <div className="pros-front">
-            <div>
-              <h4>Netflix Clone</h4>
-              <p>
-                A minimal, dark blue theme for VS Code, Sublime Text, Atom,
-                iTerm, and more. Available on Visual Studio Marketplace
-              </p>
-            </div>
-            <div className="pros-desc">
-              <div>
-                <a href="" target="_blank">
-                  <i className="fa-solid fa-arrow-up-right-from-square"></i>
-                </a>
-
-                <div id={"proj-image-gallery"} href="/">
-                  <a
-                    href="nova.png"
-                    data-pswp-width={"1320"}
-                    data-pswp-height={"614.5546"}
-                    rel="noreferrer"
-                    target="_blank"
-                  >
-                    <i className="fa-solid fa-magnifying-glass-plus"></i>
-                  </a>
-                </div>
-              </div>
-              <div>
-                <p>React</p>
-                <p>Node</p>
-                <p>Express</p>
-                <p>Git</p>
-                <p>Netlify</p>
-              </div>
-            </div>
-          </div>
-          <img className="pros-img" src="p.jpg" alt="" />
-          <div className="pros-back"></div>
-        </div>
-
-        <div className="pros">
-          <div className="pros-front">
-            <div>
-              <h4>Netflix Clone</h4>
-              <p>
-                A minimal, dark blue theme for VS Code, Sublime Text, Atom,
-                iTerm, and more. Available on Visual Studio Marketplace
-              </p>
-            </div>
-            <div className="pros-desc">
-              <div>
-                <a href="" target="_blank">
-                  <i className="fa-solid fa-arrow-up-right-from-square"></i>
-                </a>
-                <div id={"proj-image-gallery"} href="/">
-                  <a
-                    href="nova.png"
-                    data-pswp-width={"1320"}
-                    data-pswp-height={"614.5546"}
-                    rel="noreferrer"
-                    target="_blank"
-                  >
-                    <i className="fa-solid fa-magnifying-glass-plus"></i>
-                  </a>
-                </div>
-              </div>
-              <div>
-                <p>React</p>
-                <p>Node</p>
-                <p>Express</p>
-                <p>Git</p>
-                <p>Netlify</p>
-              </div>
-            </div>
-          </div>
-          <img className="pros-img" src="p2.jpg" alt="" />
+          <img className="pros-img" src="netflix-a.png" alt="" srcSet="" />
           <div className="pros-back"></div>
         </div>
       </div>
