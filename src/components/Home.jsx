a64ea3a93d5c208e7df438776116efb9d59b5a62
dagmfre <dagmfre@gmail.com>
2024-08-01 14:20:13 +0300

    Add loading at the very first page loading

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,37 @@
 import Hero from "./Hero";
-import Bg from "./Bg"
+import Bg from "./Bg";
 import About from "./About";
 import Service from "./Service";
 import Projects from "./Projects";
 import Contact from "./Contact";
 import Footer from "./Footer";
+import { useEffect, useState } from "react";
 
 function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setIsLoading(false), 5000);
+  }, []);
+
   return (
-    <div className="main-container">
-      <Hero data-aos="fade-up"/>
-      <Bg data-aos="fade-right"/>
-      <About />
-      <Service />
-      <Projects />
-      <Contact />
-      <Footer />
-    </div>
+    <>
+      {isLoading ? (
+        <div className="loader-cont">
+          <img src="loader.gif" alt=""></img>
+        </div>
+      ) : (
+        <div className="main-container">
+          <Hero data-aos="fade-up" />
+          <Bg data-aos="fade-right" />
+          <About />
+          <Service />
+          <Projects />
+          <Contact />
+          <Footer />
+        </div>
+      )}
+    </>
   );
 }
 
